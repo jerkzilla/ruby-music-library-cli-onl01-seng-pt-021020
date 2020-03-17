d238faba93b32ff7cf0d7b08649d05042f944fa8
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-03-17 15:00:31 +0000

    Automatically backed up by Learn

--- lib/music-library-controller.rb
+++ lib/music-library-controller.rb
@@ -5,42 +5,30 @@ class MusicLibraryController
   end
 
   def call
-    input = ''
-
-    while input != 'exit'
-      puts "Welcome to your music library!"
-      puts "To list all of your songs, enter 'list songs'."
-      puts "To list all of the artists in your library, enter 'list artists'."
-      puts "To list all of the genres in your library, enter 'list genres'."
-      puts "To list all of the songs by a particular artist, enter 'list artist'."
-      puts "To list all of the songs of a particular genre, enter 'list genre'."
-      puts "To play a song, enter 'play song'."
-      puts "To quit, type 'exit'."
-      puts "What would you like to do?"
+    input = ""
+    until input == "exit"
+      puts "Please enter action"
       input = gets.chomp
-
       case input
-      when 'list songs'
-        self.list_songs
-      when 'list artists'
-        self.list_artists
-      when 'list genres'
-        self.list_genres
-      when 'list artists'
-        self.list_artists
-      when 'list artist'
-        self.list_songs_by_artist
-      when 'list genre'
-        self.list_songs_by_genre
-      when 'play song'
-        self.play_song
+      when "list songs"
+        list_songs
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres
+      when "play song"
+        play_song
+      when "list artist"
+        list_artist
+      when "list genre"
+        list_genre
+      when "exit"
+        puts "Goodbye"
       else
-        "Type in a valid request please"
+        puts "invalid action"
       end
     end
-
   end
-
   def list_songs
     Song.all.sort {|a,b| a.name <=> b.name}.each.with_index(1) do |song, i|
       puts "#{i}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
@@ -63,7 +51,7 @@ class MusicLibraryController
     genres.each.with_index(1) {|genre, i| puts "#{i}. #{genre.name}"}
   end
 
-  # binding.pry
+
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
     input = gets.strip
