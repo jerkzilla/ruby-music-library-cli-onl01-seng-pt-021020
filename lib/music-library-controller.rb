9ec13b4bb23c0d0924c39ce41be43c859a8d0261
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-03-17 14:51:14 +0000

    Automatically backed up by Learn

--- lib/music-library-controller.rb
+++ lib/music-library-controller.rb
@@ -5,31 +5,41 @@ class MusicLibraryController
   end
 
   def call
-    puts "Welcome to your music library!"
-    input = ""
-    until input == "exit"
-        puts "Please enter action"
-        input = gets.chomp
-        case input
-        when "list songs"
-          list_songs
-        when "list artists"
-          list_artists
-        when "list genres"
-          list_genres
-        when "play song"
-          play_song
-        when "list artist"
-          list_artist
-        when "list genre"
-          list_genre
-        when "exit"
-          puts "Goodbye"
-        else
-          puts "invalid action"
-        end
+    input = ''
+
+    while input != 'exit'
+      puts "Welcome to your music library!"
+      puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of the genres in your library, enter 'list genres'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the songs of a particular genre, enter 'list genre'."
+      puts "To play a song, enter 'play song'."
+      puts "To quit, type 'exit'."
+      puts "What would you like to do?"
+      input = gets.chomp
+
+      case input
+      when 'list songs'
+        self.list_songs
+      when 'list artists'
+        self.list_artists
+      when 'list genres'
+        self.list_genres
+      when 'list artists'
+        self.list_artists
+      when 'list artist'
+        self.list_songs_by_artist
+      when 'list genre'
+        self.list_songs_by_genre
+      when 'play song'
+        self.play_song
+      else
+        "Type in a valid request please"
       end
     end
+
+  end
 
 
 
