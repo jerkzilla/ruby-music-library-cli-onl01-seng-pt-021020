bc8f04f35f4084a5aafd6e17dd847d44819967c8
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-03-17 14:59:16 +0000

    Automatically backed up by Learn

--- lib/music-library-controller.rb
+++ lib/music-library-controller.rb
@@ -49,7 +49,7 @@ class MusicLibraryController
   end
 
   def list_artists
-    puts "Enter artist"
+     "Enter artist"
     specific_artist = gets.chomp
     if Artist.find_by_name(specific_artist) != nil
       Artist.find_by_name(specific_artist).songs.each {|song| puts "#{song.artist.name} - #{song.name} - #{song.genre.name}"}
