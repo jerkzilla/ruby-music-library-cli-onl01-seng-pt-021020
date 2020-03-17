a01fe89c944c37808cf21ef3a135b1c11d5a1d4c
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-03-17 13:42:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,9 +3,10 @@ attr_accessor :name
 
 @@all = []
 
-  def initialize(name, artist = nil)
+  def initialize(name, artist = nil, genre = nil)
     @name = name
     self.artist=artist if artist
+    @genre = genre 
     save
   end
 
