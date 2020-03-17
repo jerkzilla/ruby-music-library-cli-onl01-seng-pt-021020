6baec4c6c8bbdb9e410d0fefa29a1dd9a7880aa1
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-03-17 14:08:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,17 +49,12 @@ attr_accessor :name
     end
   end
 
-  def self.find_by_name(name)
-    all.detect {|song| song.name == name}
+  def self.find_by_name(song)
+    self.all.detect { |s| s.name == song }
   end
 
   def self.find_or_create_by_name(song)
-      # if self.find_by_name(name)
-      #   self.find_by_name(name)
-      # else
-      #  song = self.create(name)
-      # end
-      self.find_by_name(song) || self.create(song)
+    find_by_name(song) || create(song)
   end
 
 end
