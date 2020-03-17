37e7c722b5ad9dafabcd7074aafbeb6b97256975
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-03-17 14:03:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -60,7 +60,7 @@ attr_accessor :name
        song = self.create(name)
       end
 
-      # self.find_by_name(name) || self.create(name)
+      self.find_by_name(name) || self.create(name)
   end
 
 end
