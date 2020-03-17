6e9faac534403c304d2d25f55f9fbba3ed2e5240
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-03-17 14:00:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -54,11 +54,12 @@ attr_accessor :name
   end
 
   def self.find_or_create_by_name(name)
-      if self.find_by_name(name)
-        self.find_by_name(name)
-      else
-        self.create(name)
-      end
+      # if self.find_by_name(name)
+      #   self.find_by_name(name)
+      # else
+      #   self.create(name)
+      # end
+      find_by_name(name) || create(name)
   end
 
 end
